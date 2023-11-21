382a11ea40dd097dc02965501d48d2ab13d53f1d
gustavogago <142443578+gustavogago@users.noreply.github.com>
2023-11-20 22:43:10 -0300

    Update Conversor.java

--- src/Conversor.java
+++ src/Conversor.java
@@ -1,88 +1,57 @@
-public  class Conversor {
 
-    //metodo para converter numeros em algarismos arabicos para romanos
-    String converterArabicoParaRomano(int arabico) {
+import java.util.HashMap;
+import java.util.Map;
 
-        //String em que o numero romano sera montado
+public class Conversor {
+
+    private static final Map<Character, Integer> VALORES_ROMANOS = new HashMap<>();
+    static {
+        VALORES_ROMANOS.put('I', 1);
+        VALORES_ROMANOS.put('V', 5);
+        VALORES_ROMANOS.put('X', 10);
+        VALORES_ROMANOS.put('L', 50);
+        VALORES_ROMANOS.put('C', 100);
+        VALORES_ROMANOS.put('D', 500);
+        VALORES_ROMANOS.put('M', 1000);
+    }
+
+    // Método para converter números em algarismos arábicos para romanos
+    public String converterArabicoParaRomano(int arabico) {
         String romano = "";
-
-        //Valores e simbolos que serão usados para fazer o calculo
         int[] valores = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] simbolos = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-        //Estrutura para criar a String romano
         for (int i = 0; i < valores.length; i++) {
             while (arabico >= valores[i]) {
                 romano += simbolos[i];
                 arabico -= valores[i];
-
             }
         }
 
         return romano;
     }
-    //metodo para converter numeros em algarismos romanos para arabicos
-    int converterRomanoParaArabico(String romano) {
 
-        // inteiro que ira auxiliar no calculo
-        int aux;
-
-
-        // inteiro em que o numero arabico sera montado
+    // Método para converter números em algarismos romanos para arábicos
+    public int converterRomanoParaArabico(String romano) {
         int arabico = 0;
-
-        // Caracter adcionado para evitar erros de indice
-        romano += "Ç";
-
-        // padronizando a String caso a entrada tenha letras minusculas
         romano = romano.toUpperCase();
 
+        for (int i = 0; i < romano.length(); i++) {
+            int valorAtual = VALORES_ROMANOS.get(romano.charAt(i));
 
-
-        //Estrutura para montar o numero
-        for (int i = 0; i <= romano.length()-1; i++) {
-            aux = i + 1;
-            if (romano.charAt(i) == 'M') {
-                arabico += 1000;
-
-            } else if (romano.charAt(i) == 'C' && romano.charAt(aux) == 'M') {
-                arabico += 900;
-                i++;
-            } else if (romano.charAt(i) == 'D') {
-                arabico += 500;
-            } else if (romano.charAt(i) == 'C' && romano.charAt(aux) == 'D') {
-                arabico += 400;
-                i++;
-            } else if (romano.charAt(i) == 'C') {
-                arabico += 100;
-            } else if (romano.charAt(i) == 'X' && romano.charAt(aux) == 'C') {
-                arabico += 90;
-                i++;
-            } else if (romano.charAt(i) == 'L') {
-                arabico += 50;
-            } else if (romano.charAt(i) == 'X' && romano.charAt(aux) == 'L') {
-                arabico += 40;
-                i++;
-            } else if (romano.charAt(i) == 'X') {
-                arabico += 10;
-            } else if (romano.charAt(i) == 'I' && romano.charAt(aux) == 'X') {
-                arabico += 9;
-                i++;
-            } else if (romano.charAt(i) == 'V') {
-                arabico += 5;
-            } else if (romano.charAt(i) == 'I' && romano.charAt(aux) == 'V') {
-                arabico += 4;
-                i++;
-            } else if (romano.charAt(i) == 'I') {
-                arabico ++;
+            if (i + 1 < romano.length()) {
+                int proximoValor = VALORES_ROMANOS.get(romano.charAt(i + 1));
+                if (valorAtual < proximoValor) {
+                    arabico += proximoValor - valorAtual;
+                    i++; // Avançar o índice extra para o próximo par
+                } else {
+                    arabico += valorAtual;
+                }
+            } else {
+                arabico += valorAtual;
             }
         }
 
-
         return arabico;
     }
-
 }
-
-
-
